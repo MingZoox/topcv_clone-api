d10777a1173b5eaadc6542b831ccee692adc289d
MinhVu <nguyenvanminhvu@gmail.com>
2022-10-31 18:00:29 +0700

    deploy fix

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -15,7 +15,7 @@ import { UsePipes, ValidationPipe } from "@nestjs/common";
 
 @WebSocketGateway({
   cors: {
-    origin: "http://topcv-clone.netlify.com",
+    origin: "https://topcv-clone.netlify.app",
     methods: ["GET", "POST"],
   },
 })
